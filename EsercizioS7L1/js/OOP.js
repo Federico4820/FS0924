03b824a2673ccbdee595640ed234afdd0639193c
Federico4820 <federicolepore1@gmail.com>
2024-12-09 17:02:07 +0100

    S7L1

---
+++ EsercizioS7L1/js/OOP.js
@@ -0,0 +1,31 @@
+console.log("we")
+
+let utente1={
+    Name:"Ciro",
+    Surname:"Ciruzzo",
+    Age:17
+}
+
+let utente2={
+    Name:"Cinzia",
+    Surname:"Ruotolo",
+    Age:23
+}
+
+class User{
+    constructor(x,y){
+        this.name_x = x.Name
+        this.surname_x=x.Surname
+        this.age_x=x.Age
+        this.name_y=y.Name
+        this.surname_y=y.Surname
+        this.age_y=y.Age
+        /*if(age_x>age_y){
+            return `${this.name_x} is older than ${this.name_y}`
+        }else{ return `${this.name_y} is older than ${this.name_x}`}*/
+    }
+}
+
+let confronto=new User(utente1,utente2)
+
+console.log(confronto)
