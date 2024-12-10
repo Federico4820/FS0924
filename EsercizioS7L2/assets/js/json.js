701cb5eb1d364ba854b35b7832df374b8815fc0b
Federico4820 <federicolepore1@gmail.com>
2024-12-10 16:59:10 +0100

    S7L2

--- EsercizioS7L2/assets/js/json.js
+++ EsercizioS7L2/assets/js/json.js
@@ -2,6 +2,7 @@ let nick = document.getElementById("nick");
 let btn = document.getElementById("submit");
 let hy = document.getElementById("hy");
 let del = document.getElementById("del");
+let timer = document.getElementById("timer");
 
 btn.addEventListener("click", () => {
   if (nick.value === "") {
@@ -20,3 +21,9 @@ del.addEventListener("click", () => {
   localStorage.clear();
 });
 
+setInterval(() => {
+    sec++
+    localStorage.setItem("timer",sec)
+    let localSec=localStorage.getItem("timer")
+    timer.innerText=localSec
+}, 1000);
