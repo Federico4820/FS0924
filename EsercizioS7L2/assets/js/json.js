c91792fe58430e41b74ea7178ddf8f082c796b24
Federico4820 <federicolepore1@gmail.com>
2024-12-10 16:46:16 +0100

    S7L2

--- EsercizioS7L2/assets/js/json.js
+++ EsercizioS7L2/assets/js/json.js
@@ -1,16 +1,22 @@
-let nick=document.getElementById("nick")
-let btn=document.getElementById("submit")
-let hy=document.getElementById("hy")
-let del=document.getElementById("del")
+let nick = document.getElementById("nick");
+let btn = document.getElementById("submit");
+let hy = document.getElementById("hy");
+let del = document.getElementById("del");
 
-btn.addEventListener("click", ()=>{
-    if(nick.value===""){
-    }else{let name=nick.value
-    localStorage.setItem("user",name)
-    localName=localStorage.getItem("user")
-    hy.innerText=localName}
-})
+btn.addEventListener("click", () => {
+  if (nick.value === "") {
+    localName = localStorage.getItem("user")
+    hy.innerText = `benvento ${localName}`;
+    if(localName===null){hy.innerText = ``}
+  } else {
+    let name = nick.value;
+    localStorage.setItem("user", name);
+    localName = localStorage.getItem("user");
+    hy.innerText = `benvento ${localName}`;
+  }
+});
 
-del.addEventListener("click", ()=>{
-    localStorage.clear()
-})
+del.addEventListener("click", () => {
+  localStorage.clear();
+});
+
