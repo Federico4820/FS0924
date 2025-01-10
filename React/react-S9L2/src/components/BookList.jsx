1d7536ab1bbf0a3464e36d3922df3aee85116a43
Federico4820 <federicolepore1@gmail.com>
2025-01-10 18:09:58 +0100

    S9L3

--- React/react-S9L2/src/components/BookList.jsx
+++ React/react-S9L2/src/components/BookList.jsx
@@ -1,19 +1,17 @@
-import SingleBook from "./SingleBook"
-import Row from "react-bootstrap/esm/Row"
-import Container from "react-bootstrap/esm/Container"
+import SingleBook from "./SingleBook";
+import Row from "react-bootstrap/esm/Row";
+import Container from "react-bootstrap/esm/Container";
 
-function BookList(props){
-    return(
-        <Container>
-            <Row xs={12} md={3} className="g-4">
-                {props.ArrBook.map(x => {
-                    return(
-                        <SingleBook book={x} key={x.asin}/>
-                    )
-                })}
-            </Row>
-        </Container>
-    )
+function BookList(props) {
+  return (
+    <Container>
+      <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="g-4">
+        {props.ArrBook.map((x) => {
+          return <SingleBook book={x} key={x.asin} />;
+        })}
+      </Row>
+    </Container>
+  );
 }
 
-export default BookList
+export default BookList;
--- React/react-S9L2/src/components/SingleBook.jsx
+++ React/react-S9L2/src/components/SingleBook.jsx
@@ -1,17 +1,32 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/esm/Col';
+import { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/esm/Col";
 
-function SingleBook(props) {
-  return (
-    <Col key={props.book.asin}>
-      <Card style={{}} className=" mb-4 h-100">
-        <Card.Img variant="top" src={props.book.img} className=' h-100'/>
-        <Card.Body>
-          <Card.Title>{props.book.title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+class SingleBook extends Component {
+  state = {
+    select: false,
+  };
+  render() {
+    return (
+      <Col key={this.props.book.asin}>
+        <Card
+          className=" mb-4 h-100"
+          value={this.state.select}
+          onClick={() => this.setState({ select: !this.state.select })}
+          style={{ border: this.state.select ? "3px solid red" : "none" }}
+        >
+          <Card.Img
+            variant="top"
+            src={this.props.book.img}
+            className=" h-100"
+          />
+          <Card.Body>
+            <Card.Title>{this.props.book.title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
 }
 
 export default SingleBook;
