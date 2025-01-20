6a64b6f4bf596ddb8d3d3c592fb0628d5d7c7c0b
Federico4820 <federicolepore1@gmail.com>
2025-01-20 17:01:51 +0100

    S11L1

---
+++ React/JobsSearch/src/redux/reducers/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  main: {
+    preferiti: [],
+  },
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
